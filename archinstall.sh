88d6ca90f064a3ab3e42292f07a91d36602d7def
torkzz <58431565+torkzz@users.noreply.github.com>
2024-06-25 13:19:08 -0700

    Update archinstall.sh

--- archinstall.sh
+++ archinstall.sh
@@ -3,5 +3,5 @@ systemctl mask reflector.service
 pacman-key --init
 
 echo "NTP=time.google.com" >> /etc/systemd/timesyncd.conf
-echo "FallbackNTP=0.arch.pool.ntp.org 1.arch.pool.ntp.org 2.arch.pool.ntp.org 3.arch.pool.ntp.org"
+echo "FallbackNTP=0.arch.pool.ntp.org 1.arch.pool.ntp.org 2.arch.pool.ntp.org 3.arch.pool.ntp.org" >> /etc/systemd/timesyncd.conf
 systemctl restart systemd-timesyncd

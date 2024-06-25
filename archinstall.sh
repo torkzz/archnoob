121017256e0e5aa134a751806859908ce28a5c24
torkzz <58431565+torkzz@users.noreply.github.com>
2024-06-25 12:11:45 -0700

    Update archinstall.sh

--- archinstall.sh
+++ archinstall.sh
@@ -5,3 +5,4 @@ sudo pacman -Sy pacman-contrib
 
 echo "NTP=time.google.com" >> /etc/systemd/timesyncd.conf
 systemctl restart systemd-timesyncd
+echo "FallbackNTP=0.arch.pool.ntp.org 1.arch.pool.ntp.org 2.arch.pool.ntp.org 3.arch.pool.ntp.org"
